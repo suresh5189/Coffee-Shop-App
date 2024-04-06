e3dc3d2fc8af807d517f5011fa786f8cd9c08a06
suresh5189 <suru242000@gmail.com>
2024-04-06 18:08:40 +0530

    First Commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React,{useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import PaymentScreen from './src/screens/PaymentScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import SplashScreen from 'react-native-splash-screen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
